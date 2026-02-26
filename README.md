7c786aca15f433ca2c3ad7aa71e521910f9fe91a
Adam Schafer <ams-tech@users.noreply.github.com>
2026-02-25 22:18:40 -0500

    running rpi-sign-bootcode

---
+++ README.md
@@ -0,0 +1,8 @@
+## Apps 
+
+### rpi-sign-bootcode
+
+This util is part of the [rpi-eeprom](https://github.com/raspberrypi/rpi-eeprom/blob/master/tools/rpi-sign-bootcode) repository.  It is a python script that signs the EEPROM second-stage bootloader image.
+
+`nix run .#rpi-sign-bootcode`
+
--- flake.nix
+++ flake.nix
@@ -6,14 +6,19 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }: 
   {
-      overlays = {
-        pkgs = import ./overlays/pkgs.nix;
-      };
+    overlays = {
+      pkgs = import ./overlays/pkgs.nix;
+    };
   } // flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system};
+    let 
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.pkgs ];
+      };
     in {
       # packages.default = pkgs.hello;
       # devShells.default = pkgs.mkShell { buildInputs = [ pkgs.hello ]; };
+      apps.rpi-sign-bootcode = flake-utils.lib.mkApp {drv = pkgs.rpi-sign-bootcode;};
     }
   );
 }
