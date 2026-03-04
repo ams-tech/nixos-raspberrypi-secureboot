6937203b556b5865c104771908541c66a56d7f15
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-04 13:37:54 -0500

    add metadata to test

--- checks/rpi-sb-customer-key-tests.nix
+++ checks/rpi-sb-customer-key-tests.nix
@@ -1,7 +1,9 @@
 { lib, testers }:
 testers.runNixOSTest {
   name = "key-genertion";
-  meta {
+} //
+{
+  meta = {
     description = "Test that the customer key is generated when a key does not already exist.";
     longDescription = ''
       # rpi-sb-customer-key-tests.nix
@@ -9,6 +11,6 @@ testers.runNixOSTest {
       ## Description
 
       This test verifies that the customer key is generated when a key does not already exist. It does this by running the `nixos-test` framework with a test configuration that does not include a pre-generated customer key. The test then checks for the existence of the generated key and verifies its properties.
-    ''
-  }
+    '';
+  };
 }
