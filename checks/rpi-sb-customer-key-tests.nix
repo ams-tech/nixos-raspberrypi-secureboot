eece558672d191514dcfd4e3439998d86ab985f7
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-04 14:19:11 -0500

    created nodes & simple test script

--- checks/rpi-sb-customer-key-tests.nix
+++ checks/rpi-sb-customer-key-tests.nix
@@ -1,16 +1,25 @@
 { lib, testers }:
 testers.runNixOSTest {
-  name = "key-genertion";
-} //
-{
-  meta = {
-    description = "Test that the customer key is generated when a key does not already exist.";
-    longDescription = ''
-      # rpi-sb-customer-key-tests.nix
-
-      ## Description
-
-      This test verifies that the customer key is generated when a key does not already exist. It does this by running the `nixos-test` framework with a test configuration that does not include a pre-generated customer key. The test then checks for the existence of the generated key and verifies its properties.
-    '';
+  name = "Test customer key is created correctly when an existing key is not provided.";
+  
+  # `nodes` define the VMs we spin up as part of this test.
+  nodes = {
+    # Our mock raspberry pi, which does not have an existing key provided.
+    raspberryPi = {
+      config = { config, ... }: {
+        services.rpiSbCustomerKey.enable = true;
+      };
+    };
   };
+
+  # `testScript` is a Python script using unittest-like statements.
+  # TODO: Find a link to the docs for this. https://nixos.org/manual/nixos/stable/index.html#sec-nixos-tests is close
+  testScript = ''
+    start_all()
+    raspberryPi.wait_for_unit("rpi-sb-customer-key.service")
+
+    # Check that the key was created on the raspberry pi.
+    raspberryPi.assert_file_exists("/run/secrets/rpi-sb-customer-private-key")
+    raspberryPi.assert_file_exists("/run/rpi-sb-customer-key/rpi-sb-customer-public-key")
+  '';
 }
