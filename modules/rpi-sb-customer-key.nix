7f9a5f46cbb388e436a404243393318d9271676f
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-05 10:59:03 -0500

    system starting, but for timing related purposes

--- modules/rpi-sb-customer-key.nix
+++ modules/rpi-sb-customer-key.nix
@@ -1,6 +1,6 @@
 # This module is meant to generate the customer key for the Raspberry Pi secure boot process.
 # It is not meant to be used directly, but rather as a helper for the rpi-sign-bootcode module.
-{ config, lib, ... }: 
+{ config, lib, pkgs, ... }: 
 let
 cfg = config.services.rpiSbCustomerKey; # This is how we access the configuration options for our module, which are defined in `options` below. The user will set these options in their nixOS configuration, and we can use them to customize the behavior of our module.
 in
@@ -12,6 +12,27 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    #config contents
+    systemd.services."rpi-sb-customer-key" = {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        User = "rpi-sb-customer-key";
+        WorkingDirectory = "/var/lib/rpi-sb-customer-key";
+        Type="oneshot";
+        RemainAfterExit = true;
+        ExecStart = ''
+          ${pkgs.openssl}/bin/openssl version
+          ${pkgs.openssl}/bin/openssl genrsa 2048 > /var/lib/rpi-sb-customer-key/rpi-sb-customer-private-key
+          ${pkgs.openssl}/bin/openssl rsa -in rpi-sb-customer-private-key -pubout > /var/lib/rpi-sb-customer-key/rpi-sb-customer-public-key
+        '';
+      };
+    };
+
+    users.users.rpi-sb-customer-key = {
+      home = "/var/lib/rpi-sb-customer-key";
+      createHome = true;
+      isSystemUser = true;
+      group = "rpi-sb-customer-key";
+    };
+    users.groups.rpi-sb-customer-key = { };
   };
 }
