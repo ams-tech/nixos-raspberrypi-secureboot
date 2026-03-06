a1be3252c108081b7d48592e363f7e62f04fdb0d
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-06 17:02:05 -0500

    generating keypair

--- checks/rpi-sb-customer-key-tests/generate-key.nix
+++ checks/rpi-sb-customer-key-tests/generate-key.nix
@@ -23,10 +23,9 @@ testers.runNixOSTest {
     start_all()
     raspberryPi.wait_for_unit("rpi-sb-customer-key.service")  # Wait for our service to run, which creates the key
 
-    # Check that we have a public key matching the private key.
-    # raspberryPi.succeed("openssl rsa -in /var/lib/rpi-sb-customer-key/rpi-sb-customer-private-key -pubout | grep -qf /var/lib/rpi-sb-customer-key/rpi-sb-customer-public-key")
-    raspberryPi.succeed("openssl rsa -in /var/lib/rpi-sb-customer-key/rpi-sb-customer-private-key -pubout")
     # Check that the private key is 2048 bits long
     raspberryPi.succeed("openssl rsa -in /var/lib/rpi-sb-customer-key/rpi-sb-customer-private-key -text -noout | grep 'Private-Key: (2048 bit'")
+    # Check that we have a public key matching the private key.
+    raspberryPi.succeed("openssl rsa -in /var/lib/rpi-sb-customer-key/rpi-sb-customer-private-key -pubout | grep -qf /var/lib/rpi-sb-customer-key/rpi-sb-customer-public-key")
   '';
 }
--- modules/rpi-sb-customer-key.nix
+++ modules/rpi-sb-customer-key.nix
@@ -5,26 +5,29 @@ let
 cfg = config.services.rpiSbCustomerKey; # This is how we access the configuration options for our module, which are defined in `options` below. The user will set these options in their nixOS configuration, and we can use them to customize the behavior of our module.
 in
 {
+  # options allows consumers of this module to enable/disable it programatically.
   options = {
-    services.rpiSbCustomerKey.enable = lib.mkEnableOption "Enable rpiSbCustomerKey module"; 
-    # This defines a configuration option `services.rpiSbCustomerKey.enable` that the user can set to true to enable our module. We can then check this option in our `config` to conditionally include the logic for generating the customer key.
+      # This defines a configuration option `services.rpiSbCustomerKey.enable` that the user can set to true to enable our module. We can then check this option in our `config` to conditionally include the logic for generating the customer key.
     # Note that this does not necessarily imply the systemd service is "enabled" -- this just enables the module in nixOS.
+    services.rpiSbCustomerKey.enable = lib.mkEnableOption "Enable rpiSbCustomerKey module"; 
   };
 
+  # "config" parses the options and creats our module's nixOS configuration.
   config = lib.mkIf cfg.enable {
     systemd.services."rpi-sb-customer-key" = {
       wantedBy = [ "multi-user.target" ];
       after = [ "local-fs.target" ];
-      serviceConfig = {
-        User = "rpi-sb-customer-key";
-        WorkingDirectory = "/var/lib/rpi-sb-customer-key";
+      unitConfig = {
         RequiresMountsFor = "/var/lib/rpi-sb-customer-key";
-        Type="oneshot";
+      };
+      serviceConfig = {
+        Type = "oneshot";
+        User = "rpi-sb-customer-key";
+        Group = "rpi-sb-customer-key";
+        WorkingDirectory = "/var/lib/rpi-sb-customer-key";
         RemainAfterExit = true;
         ExecStart = ''
-          ${pkgs.openssl}/bin/openssl version
-          ${pkgs.openssl}/bin/openssl genrsa 2048 > /var/lib/rpi-sb-customer-key/rpi-sb-customer-private-key
-          ${pkgs.openssl}/bin/openssl rsa -in rpi-sb-customer-private-key -pubout > /var/lib/rpi-sb-customer-key/rpi-sb-customer-public-key
+          /bin/sh -c "${pkgs.openssl}/bin/openssl genrsa 2048 > rpi-sb-customer-private-key && ${pkgs.openssl}/bin/openssl rsa -in rpi-sb-customer-private-key -pubout > rpi-sb-customer-public-key" 
         '';
       };
     };
