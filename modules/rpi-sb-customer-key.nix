a93351ad9035ccfe3580aa314490601f3d3a3efb
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-07 18:03:35 -0500

    using working directory variable in module

--- modules/rpi-sb-customer-key.nix
+++ modules/rpi-sb-customer-key.nix
@@ -5,11 +5,18 @@ let
 cfg = config.services.rpiSbCustomerKey; # This is how we access the configuration options for our module, which are defined in `options` below. The user will set these options in their nixOS configuration, and we can use them to customize the behavior of our module.
 in
 {
-  # options allows consumers of this module to enable/disable it programatically.
+  # options allows consumers of this module to enable/disable it programatically & change underlying constants.
+  # See https://nix.dev/tutorials/module-system/deep-dive for details.
   options = {
       # This defines a configuration option `services.rpiSbCustomerKey.enable` that the user can set to true to enable our module. We can then check this option in our `config` to conditionally include the logic for generating the customer key.
     # Note that this does not necessarily imply the systemd service is "enabled" -- this just enables the module in nixOS.
-    services.rpiSbCustomerKey.enable = lib.mkEnableOption "Enable rpiSbCustomerKey module"; 
+    services.rpiSbCustomerKey.enable = lib.mkEnableOption "Enable rpiSbCustomerKey Module"; 
+    # The working directory for this module.  We default this to /run because we want it to not persist through reboots -- it's a naked private key, after all!
+    services.rpiSbCustomerKey.workingDirectory = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib";
+      description = "Working directory of this service; typically something that's NOT persistent through a reboot.";
+    };
   };
 
   # "config" parses the options and creats our module's nixOS configuration.
@@ -21,7 +28,7 @@ in
         Type = "oneshot";
         User = "rpi-sb-customer-key";
         Group = "rpi-sb-customer-key";
-        WorkingDirectory = "/var/lib/rpi-sb-customer-key";
+        WorkingDirectory = "${cfg.workingDirectory}/rpi-sb-customer-key";
         RemainAfterExit = true;
         ExecStart = ''
           /bin/sh -c "${pkgs.coreutils}/bin/echo 'rpi-sb-customer-key service running'"
@@ -33,15 +40,15 @@ in
     systemd.services."rpi-sb-customer-keygen" = {
       wantedBy = [ "rpi-sb-customer-key.service" ];
       unitConfig = {
-        RequiresMountsFor = "/var/lib/rpi-sb-customer-key";
+        RequiresMountsFor = "${cfg.workingDirectory}/rpi-sb-customer-key";
         # Don't run if a private key already exists.
-        ConditionPathExists = "!/var/lib/rpi-sb-customer-key/rpi-sb-customer-private-key";
+        ConditionPathExists = "!${cfg.workingDirectory}/rpi-sb-customer-key/rpi-sb-customer-private-key";
       };
       serviceConfig = {
         Type = "oneshot";
         User = "rpi-sb-customer-key";
         Group = "rpi-sb-customer-key";
-        WorkingDirectory = "/var/lib/rpi-sb-customer-key";
+        WorkingDirectory = "${cfg.workingDirectory}/rpi-sb-customer-key";
         RemainAfterExit = true;
         ExecStart = ''
           /bin/sh -c "${pkgs.openssl}/bin/openssl genrsa 2048 > rpi-sb-customer-private-key && ${pkgs.openssl}/bin/openssl rsa -in rpi-sb-customer-private-key -pubout > rpi-sb-customer-public-key" 
@@ -50,7 +57,7 @@ in
     };
 
     users.users.rpi-sb-customer-key = {
-      home = "/var/lib/rpi-sb-customer-key";
+      home = "${cfg.workingDirectory}/rpi-sb-customer-key";
       createHome = true;
       isSystemUser = true;
       group = "rpi-sb-customer-key";
