edbeb963bad6417136b95330ebc8f4c679ff6936
Adam Schafer <ams-tech@users.noreply.github.com>
2026-03-10 21:18:31 -0400

    clean up tests -- this might all be redundant idk

--- checks/rpi-sb-customer-key-tests/sops-nix.nix
+++ checks/rpi-sb-customer-key-tests/sops-nix.nix
@@ -12,61 +12,13 @@ let
   };
 in
 {
-  sops-nix-no-configuration = rpiSbCustomerKeySopsNixTest {
-    name = "No sops-nix configuration results in service failing";
+  sops-nix-no-secret = rpiSbCustomerKeySopsNixTest {
+    name = "If sops-nix does not provide a key, the service fails.";
     extraRpiConfig = {};
     testScript = ''
       start_all()
       with t.assertRaises(AssertionError):
-        raspberryPi.wait_for_unit("rpi-sb-customer-key-sops-nix.service") 
-    '';
-  };
-  sops-nix-no-secrets = rpiSbCustomerKeySopsNixTest {
-    name = "A valid sops-nix configuration, but missing secrets file, results in service failing";
-    extraRpiConfig = {
-      services.rpiSbCustomerKey = 
-      {
-        secretsProvider = "sops-nix";
-      };
-    };
-    testScript = ''
-      start_all()
-      with t.assertRaises(AssertionError):
-        raspberryPi.wait_for_unit("rpi-sb-customer-key-sops-nix.service") 
-    '';
-  };
-    sops-nix-invalid-secrets = rpiSbCustomerKeySopsNixTest {
-    name = "A valid sops-nix configuration, but invalid secrets file, results in service failing";
-    extraRpiConfig = {
-      services.rpiSbCustomerKey = 
-      {
-        secretsProvider = "sops-nix";
-        systemd.services."rpi-sb-customer-key-test" = {
-          wantedBy = [ "rpi-sb-customer-key.service" ];
-          before = [ "rpi-sb-customer-key.service" ];
-          unitConfig = {
-            RequiresMountsFor = "/run/rpi-sb-customer-key";
-          };
-          serviceConfig = {
-            Type = "oneshot";
-            User = "rpi-sb-customer-key";
-            Group = "rpi-sb-customer-key";
-            WorkingDirectory = "/var/lib/rpi-sb-customer-key";
-            RemainAfterExit = true;
-            ExecStartPre = ''
-              /bin/sh -c "${pkgs.coreutils}/bin/mkdir -p /var/lib/rpi-sb-customer-key/" 
-              '';
-            ExecStart = ''
-              /bin/sh -c "${pkgs.coreutils}/bin/echo 'invalid secrets file' > /var/lib/rpi-sb-customer-key/secrets.yaml" 
-              '';
-          };
-        };
-      };
-    };
-    testScript = ''
-      start_all()
-      with t.assertRaises(AssertionError):
-        raspberryPi.wait_for_unit("rpi-sb-customer-key-sops-nix.service") 
+        raspberryPi.wait_for_unit("rpi-sb-customer-key-sops-nix.service")
     '';
   };
 }
